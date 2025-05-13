30df13cf47f1cba9d8361cfd2742678303a15d04
riyakakare <riyakakare@gmail.com>
2025-05-13 22:52:40 +0530

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747156946200

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -1,8 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/node_modules_next_dist_1a6ee436._.js"
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_6cdd3053._.js",
+  "static/chunks/src_components_providers_theme-provider_tsx_00534c8e._.js"
 ],
     source: "dynamic"
 });
